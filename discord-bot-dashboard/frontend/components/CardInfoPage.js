4b209cd8509d07a192d5dc217d5812b12387dbeb
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-25 15:12:02 +0530

    Added: `discord-bot-dashboard/frontend/components/CardInfoPage.js`

---
+++ discord-bot-dashboard/frontend/components/CardInfoPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CardInfoPage = () => {
+  // Add your logic here
+  
+  return (
+    <div>
+      {/* Add your JSX code here */}
+    </div>
+  );
+};
+
+export default CardInfoPage;
