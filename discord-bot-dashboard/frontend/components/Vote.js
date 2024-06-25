181fb3e480fdd1ae5da41cf94a18fa4577a2bc10
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-25 15:11:53 +0530

    Added: `discord-bot-dashboard/frontend/components/Vote.js`

---
+++ discord-bot-dashboard/frontend/components/Vote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Vote = () => {
+  // Add your logic here
+
+  return (
+    <div>
+      <h1>Vote Page</h1>
+      {/* Add your JSX elements here */}
+    </div>
+  );
+};
+
+export default Vote;
